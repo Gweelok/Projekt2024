04d18876d5b61f22889e26c090f65c81200f68d8
flaremew <salinasmaria926@gmail.com>
2021-09-08 15:27:40 +0200

    Initialized very early Dashboard and button for register item

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, ScrollView, Button, Alert, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+//                <StatusBar style="auto" />
+const ItemButton = () => {
+    const [isButton, isSetButton] = useState(true);
+    return(
+        <View>
+            <Button
+                onPress={() => {
+                    isSetButton(!isButton);
+                    }}
+                title={isButton ? "Press Me" : "Thank you!"}
+            />
+        </View>
+    );
+};
+
+const Dashboard = () => {
+    return(
+        <View style={styles.container}>
+            <ScrollView>
+                <Text>
+                    DashBoard
+                </Text>
+            </ScrollView>
+        </View>
+        );
+};
+
+function App() {
+    return (
+        <>
+            <Dashboard/>
+            <ItemButton/>
+        </>
+        );
+    };
+
+    const styles = StyleSheet.create({
+        container: {
+        flex: 1,
+        marginTop :20,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
+
+export default App
