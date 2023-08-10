2a212e86d5b7fc3db3b0a5c439f24ae4c74c6ef0
Adrian <adrian.piekarz3@gmail.com>
2023-08-10 17:03:23 +0200

    Merge fixed

--- src/componets/Uptainer.js
+++ src/componets/Uptainer.js
@@ -1,2 +1,78 @@
+import { View, Text, Image , FlatList ,StyleSheet, TouchableOpacity } from 'react-native';
+import { styles , Primarycolor1 } from '../styles/Stylesheet';
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+
+const Uptainer = ({ name, location, data }) => {
+    const navigation = useNavigation();
+//   const UptainerData = [
+//   {
+//     id: '1',
+//     name: 'Valby',
+//     location: '55.6666, 12.3000',
+//     imageSource: 'https://via.placeholder.com/200x200',
+//   },
+//   {
+//     id: '2',
+//     name: 'Norrebro',
+//     location: '55.6666, 12.1000',
+//     imageSource: 'https://via.placeholder.com/200x200',
+//   },
+//   {
+//     id: '3',
+//     name: 'Norrebro',
+//     location: '55.6666, 12.000',
+//     imageSource: 'https://via.placeholder.com/200x200',
+//   },
+// ]
+
+return (
+    <View> 
+      <Text style={styles.menuItem_text}> {name}</Text>
+        <Text style={{fontSize :18 , color :Primarycolor1}}> {location}</Text>
+        <FlatList
+      horizontal={true}
+      data={data}
+      keyExtractor={(item) => item.id}
+      style={{marginBottom:5, marginTop:5,}}
+      renderItem={({ item }) => (
+        <TouchableOpacity onPress={() => navigation.navigate('DetailView', {data: item })}>
+        <View style={styling.item}>
+            <Image source={{ uri: item.imageSource }} style={styling.image} />
+        </View>
+        </TouchableOpacity>
+      )}
+    />
+     <FlatList
+      horizontal={true}
+      data={data}
+      keyExtractor={(item) => item.id}
+      style={{marginBottom:10, marginTop:5,}}
+      renderItem={({ item }) => (
+        <TouchableOpacity  onPress={() => navigation.navigate('DetailView', {data: item })}>
+        <View style={styling.item}>
+            <Image source={{ uri: item.imageSource }} style={styling.image} />
+        </View>
+        </TouchableOpacity>
+      )}
+    />
+    </View>
+);
+}
+const styling = StyleSheet.create({
+    item: {
+      width: 100, // Set the width of each item
+      height: 100,
+      margin: 5,
+      borderRadius: 10,
+      overflow: 'hidden',
+    },
+    image: {
+      width: '100%',
+      height: '100%',
+      resizeMode: 'cover',
+    },
+  });
 
 export default Uptainer;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,26 +1,116 @@
-import { View, Text} from 'react-native';
-import { styles , Backgroundstyle} from '../styles/Stylesheet';
-import Navigationbar from '../componets/Navigationbar';
-import React from 'react';
+import { View, TextInput, ScrollView, StyleSheet } from "react-native";
+import { styles, Backgroundstyle } from "../styles/Stylesheet";
+import Navigationbar from "../componets/Navigationbar";
+import React, { useState } from "react";
+import Uptainer from "../componets/Uptainer";
+import { BoxLink } from "../styles/BoxLink";
 import * as Location from "expo-location";
+import SortUptainers from "../componets/sortUptainers";
+
+///Asumming i have data
+const photo = [
+  { id: "1", imageSource: "https://via.placeholder.com/200x200" },
+  { id: "2", imageSource: "https://via.placeholder.com/200x200" },
+  { id: "3", imageSource: "https://via.placeholder.com/200x200" },
+  { id: "4", imageSource: "https://via.placeholder.com/200x200" },
+  { id: "5", imageSource: "https://via.placeholder.com/200x200" },
+  { id: "6", imageSource: "https://via.placeholder.com/200x200" },
+  { id: "7", imageSource: "https://via.placeholder.com/200x200" },
+  { id: "8", imageSource: "https://via.placeholder.com/200x200" },
+  { id: "9", imageSource: "https://via.placeholder.com/200x200" },
+  // Add more image URLs as needed
+];
+
+const UptainerData = [
+  {
+    name: "Valby",
+    location: "Allegrade",
+    photos: photo,
+    latitude: 55.6666,
+    longitude: 12.1,
+  },
+
+  {
+    name: "Norrebo",
+    location: "ved fatka",
+    photos: photo,
+    latitude: 55.6666,
+    longitude: 12.2,
+  },
+  {
+    name: "Norrebo",
+    location: "ved fatka",
+    photos: photo,
+    latitude: 55.6666,
+    longitude: 12.3,
+  },
+];
+
+const renderUptainers = () => {
+  // Create a new array without the first element
+  const slicedUptainerData = UptainerData.slice(1);
+
+  // rendering the rest of the Uptainer components
+  return slicedUptainerData.map((item, index) => (
+    <Uptainer
+      key={index + 1}
+      name={item.name}
+      location={item.location}
+      data={item.photos}
+    />
+  ));
+};
 
 const Home = ({ navigation }) => {
+  const startBackgroundTracking = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status == "granted") {
+      await Location.requestBackgroundPermissionsAsync();
+    }
+  };
 
-  
-    const startBackgroundTracking = async () => {
-        const { status } = await Location.requestForegroundPermissionsAsync();
-        if (status == "granted") {
-          await Location.requestBackgroundPermissionsAsync();
-        }
-      };
+  const [search, onChangeSearch] = useState("");
 
+  const handleImagePress = () => {
+    navigation.navigate('DetailView'); 
+  };
 
-return (
-    <View style={Backgroundstyle.interactive_screens}> 
-        <Text style={styles.Header_Primarycolor1}> Home page </Text>
-        <Navigationbar navigation={navigation}/>
+  return (
+    <View style={Backgroundstyle.interactive_screens}>
+      <TextInput
+        placeholder="search"
+        value={search}
+        onChangeText={onChangeSearch}
+        keyboardType="email-address"
+        autoCapitalize="none"
+        clearButtonMode={"always"}
+        style={[styles.inputBox, style.searchbar]}
+      />
+      <ScrollView style={{ marginBottom: 60 }}>
+        <Uptainer
+          name={UptainerData[0].name}
+          location={UptainerData[0].location}
+          data={UptainerData[0].photos}
+          navigation={navigation}
+        />
+        <SortUptainers />
+        <Uptainer
+          name={UptainerData[0].name}
+          location={UptainerData[0].location}
+          data={UptainerData[0].photos}
+        />
+        <BoxLink msg="Hvordan funger UPDROPP?" />
+        {renderUptainers()}
+      </ScrollView>
+      <Navigationbar navigation={navigation} />
     </View>
   );
 };
 
 export default Home;
+
+const style = StyleSheet.create({
+  searchbar: {
+    marginTop: 10,
+  },
+});
