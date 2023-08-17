6dd363d32cd7b857f6d060d12d02852ecc9965cc
Arinze <narinzeanthony@yahoo.com>
2023-08-17 07:52:20 +0100

    privacy policy

--- App.js
+++ App.js
@@ -33,31 +33,33 @@ import {
 } from "./src/componets/molocules/registerOptions";
 import { DashboardScreen } from "./src/screens/dashboardScreen";
 import { ProductScreen } from "./src/screens/productScreen";
-import SignUpScreen from "./src/screens/SignUpScreen"
+import SignUpScreen from "./src/screens/SignUpScreen";
 import { View } from "react-native-ui-lib";
-import { useFonts } from 'expo-font';
+import { useFonts } from "expo-font";
 //importing pages for navigation
 import Home from "./src/screens/Home";
 import Map from "./src/screens/map/Map";
 import Profile from "./src/screens/Profile";
 import Add from "./src/screens/Add";
 import Stat from "./src/screens/Stat";
-import DetailView from "./src/screens/DetailView"
+import DetailView from "./src/screens/DetailView";
 import TermsAndConditions from "./src/screens/TermsAndConditions";
 import ProfileCreated from "./src/screens/ProfileCreated";
-import LandingScreen from "./src/screens/LandingScreen"
-import { useLanguage, LanguageProvider, t } from './src/Languages/LanguageHandler';
+import LandingScreen from "./src/screens/LandingScreen";
+import {
+  useLanguage,
+  LanguageProvider,
+  t,
+} from "./src/Languages/LanguageHandler";
 import StationDetailScreen from "./src/screens/map/stationDetail/StationDetailScreen";
 import Info from "./src/screens/Info";
 import SignIn from "./src/screens/SignIn";
 import ArticlePage from "./src/screens/article/ArticlePage";
+import PrivacyPolicy from "./src/screens/PrivacyPolicy";
 //import { seedCheck } from "./src/utils/FirebaseSeed"; //uncomment to seed data(only works if DB is empty)
 
 console.log("start");
 const Stack = createNativeStackNavigator();
-
-
-
 
 // Main function that everything runs in
 export default function App() {
@@ -71,15 +73,15 @@ export default function App() {
     //dropAll();
     //createTestData();
   }, []);
-  
+
   //FirebaseSeed data here:
   //seedCheck(); //uncomment to seed data(only works if DB is empty)
 
   //Loading the font
   const [fontsLoaded] = useFonts({
-    'space-grotesk': require('./assets/fonts/SpaceGrotesk-Regular.ttf'),
-    'space-grotesk-bold': require('./assets/fonts/SpaceGrotesk-Bold.ttf'),
-    'space-grotesk-Medium': require('./assets/fonts/SpaceGrotesk-Medium.ttf'),
+    "space-grotesk": require("./assets/fonts/SpaceGrotesk-Regular.ttf"),
+    "space-grotesk-bold": require("./assets/fonts/SpaceGrotesk-Bold.ttf"),
+    "space-grotesk-Medium": require("./assets/fonts/SpaceGrotesk-Medium.ttf"),
   });
 
   if (!fontsLoaded) {
@@ -89,110 +91,108 @@ export default function App() {
   // Main navigation of all the views
   return (
     <LanguageProvider>
-    <NavigationContainer theme={navStyle}>
-      <Stack.Navigator initialRouteName="Landingscreen">
-      <Stack.Screen
-          name="Landingscreen"
-          component={LandingScreen}
-          options={{animation : "none"}}
-        />
+      <NavigationContainer theme={navStyle}>
+        <Stack.Navigator initialRouteName="Landingscreen">
+          <Stack.Screen
+            name="Landingscreen"
+            component={LandingScreen}
+            options={{ animation: "none" }}
+          />
 
-        <Stack.Screen
-          name="SignUp"
-          component={SignUpScreen}
-          options={{animation : "none"}}
-        />
-        <Stack.Screen
+          <Stack.Screen
+            name="SignUp"
+            component={SignUpScreen}
+            options={{ animation: "none" }}
+          />
+          <Stack.Screen
             name="Sign in"
             component={SignIn}
-            options={{animation : "none", headerLeft: null}}
-        />
-        <Stack.Screen
-          name="Homepage"
-          component={Home}
-          options={{animation : "none"}}
-        />
-        <Stack.Screen 
-          name="DetailView"
-          component={DetailView} />
-        <Stack.Screen
-          name="Infopage"
-          component={ArticlePage}
-          options={{ headerShown: false, animation : "none" }}
+            options={{ animation: "none", headerLeft: null }}
+          />
+          <Stack.Screen
+            name="Homepage"
+            component={Home}
+            options={{ animation: "none" }}
+          />
+          <Stack.Screen name="DetailView" component={DetailView} />
+          <Stack.Screen
+            name="Infopage"
+            component={ArticlePage}
+            options={{ headerShown: false, animation: "none" }}
+          />
+          <Stack.Screen
+            name="Map"
+            component={Map}
+            options={{ animation: "none", headerShown: false }}
+          />
+          <Stack.Screen
+            name="Profile"
+            component={Profile}
+            options={{ animation: "none" }}
+          />
+          <Stack.Screen
+            name="TermsAndConditions"
+            component={TermsAndConditions}
+            options={{ animation: "none" }}
+          />
+          <Stack.Screen
+            name="ProfileCreated"
+            component={ProfileCreated}
+            options={{ animation: "none" }}
+          />
+          <Stack.Screen
+            name="Stats"
+            component={Stat}
+            options={{ animation: "none" }}
+          />
+          <Stack.Screen
+            name="Add"
+            component={Add}
+            options={{ animation: "none" }}
+          />
+          <Stack.Screen
+            name="Products"
+            component={ProductScreen}
+            options={{ title: "Products", animation: "none" }}
+          />
 
-        />
-        <Stack.Screen
-          name="Map"
-          component={Map}
-          options={{animation : "none", headerShown: false}}
-        />
-        <Stack.Screen
-          name="Profile"
-          component={Profile}
-          options={{animation : "none"}}
-        />
-        <Stack.Screen
-        name="TermsAndConditions"
-        component={TermsAndConditions}
-        options={{animation : "none"}}
-        />
-        <Stack.Screen
-          name="ProfileCreated"
-          component={ProfileCreated}
-          options={{animation : "none"}}
-        />
-        <Stack.Screen
-          name="Stats"
-          component={Stat}
-          options={{animation : "none"}}
-        />
-        <Stack.Screen
-          name="Add"
-          component={Add}
-          options={{animation : "none"}}
-        />
-        <Stack.Screen
-          name="Products"
-          component={ProductScreen}
-          options={{ title: "Products", animation : "none"}}
-        />
-
-        <Stack.Screen
-          name="Pro"
-          component={ProScreen}
-          options={{ title: "Products" , animation : "none"}}
-        />
-        <Stack.Screen
-          name="Bnd"
-          component={BndScreen}
-          options={{ title: "Brands" , animation : "none" }}
-        />
-        <Stack.Screen
-          name="Mod"
-          component={ModScreen}
-          options={{ title: "Models" , animation : "none" }}
-        />
-        <Stack.Screen
-            name="Stations"
-            component={StationsScreen}
-        />
-        <Stack.Screen
+          <Stack.Screen
+            name="Pro"
+            component={ProScreen}
+            options={{ title: "Products", animation: "none" }}
+          />
+          <Stack.Screen
+            name="Bnd"
+            component={BndScreen}
+            options={{ title: "Brands", animation: "none" }}
+          />
+          <Stack.Screen
+            name="Mod"
+            component={ModScreen}
+            options={{ title: "Models", animation: "none" }}
+          />
+          <Stack.Screen name="Stations" component={StationsScreen} />
+          <Stack.Screen
             name="StationDetails"
             component={StationDetailScreen}
             options={{ headerShown: false }} // hides the header
-        />
-        <Stack.Screen
-          name="Thanks"
-          component={ThanksScreen}
-          options={{ headerShown: false , animation : "none"}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+          />
+          <Stack.Screen
+            name="Thanks"
+            component={ThanksScreen}
+            options={{ headerShown: false, animation: "none" }}
+          />
+          <Stack.Screen
+            name="PrivacyPolicy"
+            component={PrivacyPolicy}
+            options={{ headerShown: true, animation: "none" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </LanguageProvider>
   );
 
   //Screens
-
 
   // eslint-disable-next-line react/prop-types
   function StationsScreen({ navigation, route }) {
@@ -243,4 +243,3 @@ export default function App() {
     );
   }
 }
-
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,16 +1,45 @@
-import { View, Text} from 'react-native';
-import { styles ,Backgroundstyle} from '../styles/Stylesheet';
-import Navigationbar from '../componets/Navigationbar';
-import React from 'react';
-
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { styles, Backgroundstyle } from "../styles/Stylesheet";
+import Navigationbar from "../componets/Navigationbar";
+import React from "react";
+import { windowWidth } from "../utils/Dimensions";
+import { Ionicons } from "@expo/vector-icons";
 
 const Profile = ({ navigation }) => {
-return (
-    <View style={Backgroundstyle.interactive_screens}> 
-        <Text style={styles.Header_Primarycolor1}> Profile page </Text>
-        <Navigationbar navigation={navigation}/>
+  return (
+    <View style={Backgroundstyle.interactive_screens}>
+      <View style={{ padding: 20 }}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("PrivacyPolicy")}
+          style={{
+            borderWidth: 3,
+            borderColor: styles.menuItem.borderColor,
+            padding: 20,
+            width: windowWidth / 1.1,
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text
+            style={{
+              color: styles.menuItem.borderColor,
+              fontSize: 16,
+              fontWeight: "900",
+            }}
+          >
+            Data Policy
+          </Text>
+          <Ionicons
+            name="chevron-forward"
+            color={styles.menuItem.borderColor}
+            size={20}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <Navigationbar navigation={navigation} />
     </View>
-);
-}
+  );
+};
 
 export default Profile;
