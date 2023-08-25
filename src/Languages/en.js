318d83a815daa6d62b8f3db7000ac12c709bc7f4
Arinze <narinzeanthony@yahoo.com>
2023-08-25 10:31:54 +0100

    informative text task

--- src/Languages/en.js
+++ src/Languages/en.js
@@ -1,58 +1,58 @@
 export default {
-    SignUpScreen : {
-        Signup: 'Sign up',
-        password : "Password",
-        passwordmsg :"min. eight characters",
-        LogInLink : "Already have an account? Log in"
-    },
-    LandingScreen : {
-        LanguageSelector : 'Danish' ,
-        Header : "Welcome to Updropp",
-        Intro : "A lot of the earth's raw materials are used to produce electronics. Updropp turns waste into resources, and reduces the need for extraction of new resources.",
-        Littlemsg:"By pressing continue, you accept our",
-        Termsandcond : "Terms and Conditions",
-        continue : "Continue",
-    },
-    LogoutConfirmation: {
-        confirmMessage: "Are you sure you want to log out?",
-        logoutButton: "Log Out",
-        cancelButton: "Cancel"
-    },
-    Profile: {
-        logout: "Log Out"
-    },
-    SignInScreen : {
-        Headline: "Sign in",
-        Button: "Sign in",
-        ForgetPwHint:"Forgot your password?",
-        SignUpHint:  "Don't have a user? Sign up.",
-    },
+  SignUpScreen: {
+    Signup: "Sign up",
+    password: "Password",
+    passwordmsg: "min. eight characters",
+    LogInLink: "Already have an account? Log in",
+  },
+  LandingScreen: {
+    LanguageSelector: "Danish",
+    Header: "Welcome to Updropp",
+    Intro:
+      "A lot of the earth's raw materials are used to produce electronics. Updropp turns waste into resources, and reduces the need for extraction of new resources.",
+    Littlemsg: "By pressing continue, you accept our",
+    Termsandcond: "Terms and Conditions",
+    continue: "Continue",
+  },
+  LogoutConfirmation: {
+    confirmMessage: "Are you sure you want to log out?",
+    logoutButton: "Log Out",
+    cancelButton: "Cancel",
+  },
+  Profile: {
+    logout: "Log Out",
+  },
+  SignInScreen: {
+    Headline: "Sign in",
+    Button: "Sign in",
+    ForgetPwHint: "Forgot your password?",
+    SignUpHint: "Don't have a user? Sign up.",
+  },
 
-    ProfileScreen: {
-        ProfilePage: "Edit Profile",
-        MySettings: "My Settings",
-        MyDrafts: "My Drafts",
-        DataPolicy: "Data Policy",
-        ContactUs: "Contact Us",
-    },
+  ProfileScreen: {
+    ProfilePage: "Edit Profile",
+    MySettings: "My Settings",
+    MyDrafts: "My Drafts",
+    DataPolicy: "Data Policy",
+    ContactUs: "Contact Us",
+  },
 
-    UpdroppForm: {
-        title: "Updropp",
-    },
-    CustomInput:{
-        hint: "optional",
-    },
+  UpdroppForm: {
+    title: "Updropp",
+    imformativeText:
+      "To Updropp your item you need to scan the QR-code on the Uptainer, when you drop off the item. If you have added information beforehand, you can save a draft by pressing the 'Scan later' button ",
+  },
+  CustomInput: {
+    hint: "optional",
+  },
 
-
-
-
-    NotificationsScreen : {
-        Header: "Notifications",
-    },
-    AccountSettingsScreen : {
-        Header: "Account Settings",
-    },
-    MySettingsScreen : {
-        Header: "My Settings",
-    },
-  };
+  NotificationsScreen: {
+    Header: "Notifications",
+  },
+  AccountSettingsScreen: {
+    Header: "Account Settings",
+  },
+  MySettingsScreen: {
+    Header: "My Settings",
+  },
+};
