87bec6d93534cde06f0cdc9a9f0d230b39350367
Adrian <adrian.piekarz3@gmail.com>
2023-08-25 15:16:27 +0200

    [feat] description field added

--- src/screens/form/DescriptionField.js
+++ src/screens/form/DescriptionField.js
@@ -2,14 +2,14 @@ import {View, Text, TextInput, StyleSheet} from 'react-native';
 import React from 'react';
 import { useLanguage, t } from '../../Languages/LanguageHandler';
 import { AddStyles } from '../Add';
-import { descriptionFieldStyles } from "../../styles/Stylesheet";
+import { descriptionFieldStyles, Backgroundstyle } from "../../styles/Stylesheet";
 
 
 const DescriptionField = ({ label }) => {
     const { currentLanguage } = useLanguage();
 
     return (
-        <View>
+        <View style={Backgroundstyle.interactive_screens}>
         <Text style={descriptionFieldStyles.dscLabel}> {label} {t('DescriptionField.label', currentLanguage)}</Text>
             <TextInput
                 style={descriptionFieldStyles.dscInput}
