0767da709db9edf9a4479b35c835bb556a569fc2
ceimiplace <azamfiredenis@yahoo.com>
2023-08-26 23:30:47 +0300

    removed navigation for later

--- src/screens/LandingScreen.js
+++ src/screens/LandingScreen.js
@@ -48,27 +48,6 @@ const LanguageSelector = () => {
     }
 
 }; 
-useEffect(() => {
-    const backAction = () => {
-      Alert.alert('Hold on!', 'Are you sure you want to go back?', [
-        {
-          text: 'Cancel',
-          onPress: () => null,
-          style: 'cancel',
-        },
-        {text: 'YES', onPress: () => BackHandler.exitApp()},
-      ]);
-      return true;
-    };
-
-    const backHandler = BackHandler.addEventListener(
-      'hardwareBackPress',
-      backAction,
-    );
-
-    return () => backHandler.remove();
-  }, []);
-
 
 return (
     <View style={Backgroundstyle.informationScreens}> 
