368e22ca4ea861aa77d435807b222d17dd3f5cc2
Arinze <narinzeanthony@yahoo.com>
2023-09-06 07:20:21 +0100

    fixed the overflowing text on the uptainer details screen

--- src/screens/UptainerDetails.js
+++ src/screens/UptainerDetails.js
@@ -11,9 +11,14 @@ import {
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import Navigationbar from "../componets/Navigationbar";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
-import { getItemsInUptainer, getUptainerById, getProductById, getBrandById} from '../utils/Repo';
+import {
+  getItemsInUptainer,
+  getUptainerById,
+  getProductById,
+  getBrandById,
+} from "../utils/Repo";
 import GlobalStyle from "../styles/GlobalStyle";
 
 const windowWidth = Dimensions.get("window").width;
@@ -22,31 +27,41 @@ const windowHeight = Dimensions.get("window").height;
 const UptainerDetails = ({ navigation, route }) => {
   const item = route.params;
   const [data, setData] = useState([]);
-  const [uptainerImageUrl, setUptainerImageUrl] = useState(''); // New state for Uptainer image URL
+  const [uptainerImageUrl, setUptainerImageUrl] = useState(""); // New state for Uptainer image URL
 
-  useEffect(() => {  //Fetches items in the uptainer 
+  useEffect(() => {
+    //Fetches items in the uptainer
     const fetchItemList = async () => {
       const storage = getStorage();
       try {
         const items = await getItemsInUptainer(item.id); // Assuming 'id' is defined somewhere --> id is from Uptainer (ln 42)
-        const updatedData = await Promise.all(items.map(async (item) => {
-          const pathReference = ref(storage, item.itemImage); // Adjust the path according to your storage structure
-          const product = await getProductById(item.itemproduct);
-          const brand = await getBrandById(item.itemBrand);
-          
-          
-          try {
-            const url = await getDownloadURL(pathReference);
-            
-            return { ...item, imageUrl: url,  productName: product.productName, brandName: brand.brandName,};
-          } catch (error) {
-            console.log('Error while downloading image => ', error);
-            return { ...item, imageUrl: 'https://via.placeholder.com/200x200' };
-          }
-        }));
+        const updatedData = await Promise.all(
+          items.map(async (item) => {
+            const pathReference = ref(storage, item.itemImage); // Adjust the path according to your storage structure
+            const product = await getProductById(item.itemproduct);
+            const brand = await getBrandById(item.itemBrand);
+
+            try {
+              const url = await getDownloadURL(pathReference);
+
+              return {
+                ...item,
+                imageUrl: url,
+                productName: product.productName,
+                brandName: brand.brandName,
+              };
+            } catch (error) {
+              console.log("Error while downloading image => ", error);
+              return {
+                ...item,
+                imageUrl: "https://via.placeholder.com/200x200",
+              };
+            }
+          })
+        );
         setData(updatedData); // updates data property with the fetched data from db
       } catch (error) {
-        console.log('Error while fetching items => ', error);
+        console.log("Error while fetching items => ", error);
       }
     };
     fetchItemList();
@@ -60,19 +75,18 @@ const UptainerDetails = ({ navigation, route }) => {
     fetchUptainerImage();
   }, []);
 
-async function getUptainerImageUrl() { //get uptainerUrl from database
+  async function getUptainerImageUrl() {
+    //get uptainerUrl from database
     const storage = getStorage();
     try {
-        const currentUptainer = await getUptainerById(item.id);
-        const uptainerPathReference = ref(storage, currentUptainer.uptainerImage);
-        return await getDownloadURL(uptainerPathReference);
-        
+      const currentUptainer = await getUptainerById(item.id);
+      const uptainerPathReference = ref(storage, currentUptainer.uptainerImage);
+      return await getDownloadURL(uptainerPathReference);
     } catch (error) {
-        console.log('Error while getting Uptainer Image URL => ', error);
-        return 'https://via.placeholder.com/200x200';
+      console.log("Error while getting Uptainer Image URL => ", error);
+      return "https://via.placeholder.com/200x200";
     }
-}
-  
+  }
 
   return (
     // <View style={GlobalStyle.BodyWrapper}>
@@ -91,7 +105,7 @@ async function getUptainerImageUrl() { //get uptainerUrl from database
           <ImageBackground
             style={styles.detailsImage}
             source={{
-              uri: uptainerImageUrl,  // current uptainer main pic 
+              uri: uptainerImageUrl, // current uptainer main pic
             }}
           >
             <TouchableOpacity
@@ -115,37 +129,49 @@ async function getUptainerImageUrl() { //get uptainerUrl from database
             padding: 10,
           }}
         >
-          {data?.map((cur, i) => (  // loads item images contained in the uptainer to the screen
-            <TouchableOpacity
-              key={i}
-              style={{
-                marginLeft: 6,
-                marginBottom: 20,
-                marginRight: 20,
-                alignContent: "center",
-                alignItems: "center",
-                alignSelf: "center",
-                justifyContent: "center",
-              }}
-              onPress={() => navigation.navigate("DetailView", {
-                itemDescription: cur.itemDescription,
-                imageUrl: cur.imageUrl,
-                productName: cur.productName,
-                brandName: cur.brandName,
-                
-
-              }
-              )}
-            >
-              <Image
-                style={styles.moreProductsImage}
-                source={{
-                  uri: cur?.imageUrl,
+          {data?.map(
+            (
+              cur,
+              i // loads item images contained in the uptainer to the screen
+            ) => (
+              <TouchableOpacity
+                key={i}
+                style={{
+                  marginLeft: 6,
+                  marginBottom: 20,
+                  marginRight: 20,
+                  alignContent: "center",
+                  alignItems: "center",
+                  alignSelf: "center",
+                  justifyContent: "center",
                 }}
-              />
-              <Text style={{ fontWeight: "600" }}>{cur.productName} </Text> 
-            </TouchableOpacity>
-          ))}
+                onPress={() =>
+                  navigation.navigate("DetailView", {
+                    itemDescription: cur.itemDescription,
+                    imageUrl: cur.imageUrl,
+                    productName: cur.productName,
+                    brandName: cur.brandName,
+                  })
+                }
+              >
+                <Image
+                  style={styles.moreProductsImage}
+                  source={{
+                    uri: cur?.imageUrl,
+                  }}
+                />
+                <Text
+                  style={{
+                    fontWeight: "600",
+                    width: windowWidth / 2.7,
+                    textAlign: "center",
+                  }}
+                >
+                  {cur.productName}{" "}
+                </Text>
+              </TouchableOpacity>
+            )
+          )}
         </View>
         <View
           style={[
@@ -156,7 +182,7 @@ async function getUptainerImageUrl() { //get uptainerUrl from database
       </ScrollView>
       <Navigationbar navigation={navigation} />
     </View>
-     // </View>
+    // </View>
   );
 };
 
