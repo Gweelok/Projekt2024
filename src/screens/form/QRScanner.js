881bf9c7f8dadda7e6a67facd704c4ede25fc350
Amr <paadd2@hotmail.com>
2023-09-07 05:54:58 +0200

    display scanned qr, and clear text + data

--- src/screens/form/QRScanner.js
+++ src/screens/form/QRScanner.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, Linking, Button } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, Button } from "react-native";
 import { t, useLanguage } from "../../Languages/LanguageHandler";
 import { useNavigation } from "@react-navigation/native";
-import Icon from "react-native-vector-icons/FontAwesome"; // Import the icon library you want to use
+import Icon from "react-native-vector-icons/FontAwesome";
 import { BarCodeScanner } from 'expo-barcode-scanner';
-
-
+import { generateQRCode } from '../../utils/QRCodeGenerator';
 
 const QRScanner = () => {
     const navigation = useNavigation();
@@ -13,111 +12,121 @@ const QRScanner = () => {
 
     const handlePress = () => {
         navigation.goBack();
-}
+    }
 
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+    const [text, setText] = useState('Not yet scanned');
+    const [scannedQRCode, setScannedQRCode] = useState(null); // State to store the generated QR code
 
-const [hasPermission, setHasPermission] = useState(null);
-const [scanned, setScanned] = useState(false);
-const [text, setText] = useState('Not yet scanned');
+    const askForCameraPermission = () => {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }
 
-const askForCameraPermission = () => {
-    (async () => {
-        const { status } = await BarCodeScanner.requestPermissionsAsync();
-        setHasPermission(status === 'granted');
-    })();
-}
+    // Request Camera Permission
+    useEffect(() => {
+        askForCameraPermission();
+    }, []);
 
-// Request Camera Permission
-useEffect(() => {
-    askForCameraPermission();
-}, []);
+    // What happens when we scan the barcode
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        setText(data);
+        const scannedQRCode = generateQRCode(data); // Generate QR code for scanned data
+        setScannedQRCode(scannedQRCode); // Set the generated QR code for display
+        console.log('Type: ' + type + '\nData: ' + data)
+    };
 
-// What happens when we scan the bar code
-const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    setText(data);
-    console.log('Type: ' + type + '\nData: ' + data)
-};
+    // Function to handle the "Scan again?" button press
+    const handleScanAgain = () => {
+        setScanned(false); // Reset the scanned state
+        setText('Not yet scanned'); // Reset the scanned text
+        setScannedQRCode(null); // Clear the scanned QR code
+    };
 
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.header}>
+                {/* Header Text */}
+                <Text style={styles.headline}>
+                    {t("QrScannerScreen.Scan", currentLanguage)}
+                </Text>
+                {/* Close Button */}
+                <TouchableOpacity style={styles.closeButton} onPress={handlePress}>
+                    <Icon size={30} name="close" style={styles.closeButtonIcon} />
+                </TouchableOpacity>
+            </View>
 
+            <View style={styles.content}>
+                {/* Header Instruction */}
+                <Text style={styles.instruction}>
+                    {t("QrScannerScreen.Header", currentLanguage)}
+                </Text>
 
+                {/* QR Code Scanner Frame */}
+                {hasPermission ? (
+                    <View style={styles.qrScannerFrame}>
+                        <BarCodeScanner
+                            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                            style={{ height: 320, width: 400 }}
+                        />
+                    </View>
+                ) : (
+                    <Text style={{ margin: 10 }}>No access to the camera</Text>
+                )}
 
-  
-return (
-    <SafeAreaView style={styles.container}>
-        <View style={styles.header}>
-            {/* Header Text */}
-            <Text style={styles.headline}>
-                {t("QrScannerScreen.Scan", currentLanguage)}
-            </Text>
-            {/* Close Button */}
-            <TouchableOpacity style={styles.closeButton} onPress={handlePress}>
-                <Icon size={30} name="close" style={styles.closeButtonIcon} />
-            </TouchableOpacity>
-        </View>
+                {/* Display scanned text */}
+                <Text style={styles.maintext}>{text}</Text>
 
-        <View style={styles.content}>
-            {/* Header Instruction */}
-            <Text style={styles.instruction}>
-                {t("QrScannerScreen.Header", currentLanguage)}
-            </Text>
+                {/* Display generated QR code */}
+                {scannedQRCode && (
+                    <View style={styles.qrCodeContainer}>
+                        {scannedQRCode}
+                    </View>
+                )}
 
-            {/* QR Code Scanner Frame */}
-            {hasPermission ? (
-                <View style={styles.qrScannerFrame}>
-                    <BarCodeScanner
-                        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-                        style={{ height: 320, width: 400 }}
-                    />
-                </View>
-            ) : (
-                <Text style={{ margin: 10 }}>No access to camera</Text>
-            )}
+                {scanned && (
+                    <Button title={'Scan again?'} onPress={handleScanAgain} color='darkgreen' />
+                )}
 
-            {/* Display scanned text */}
-            <Text style={styles.maintext}>{text}</Text>
-
-            {scanned && (
-                <Button title={'Scan again?'} onPress={() => setScanned(false)} color='darkgreen' />
-            )}
-
-            {/* Instruction for Non-Uptainers */}
-            <Text style={styles.instruction}>
-                {t("QrScannerScreen.Bottom", currentLanguage)}
-            </Text>
-        </View>
-    </SafeAreaView>
-);
-
-
+                {/* Instruction for Non-Uptainers */}
+                <Text style={styles.instruction}>
+                    {t("QrScannerScreen.Bottom", currentLanguage)}
+                </Text>
+            </View>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "white", // White background
+        backgroundColor: "white",
     },
     header: {
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
-        paddingVertical: 10, // Added padding to lower the header slightly
+        paddingVertical: 10,
         paddingHorizontal: 5,
-        backgroundColor: "white", // White background
-        marginTop: 15, // Adjust the margin to lower the header
+        backgroundColor: "white",
+        marginTop: 15,
     },
     closeButton: {
         backgroundColor: "darkgreen",
-        padding: 5, // Decreased padding to make the button smaller
+        padding: 5,
         borderRadius: 5,
     },
     closeButtonIcon: {
-        color: "white", // White text color
+        color: "white",
     },
     headline: {
         fontSize: 24,
         fontWeight: "bold",
-        color: "darkgreen", // Dark green text color
+        color: "darkgreen",
     },
     content: {
         flex: 1,
@@ -130,20 +139,18 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         marginBottom: 10,
-        //  backgroundColor: "darkgreen", // Dark green background for border
-        borderRadius: 10, // Rounded corners
+        borderRadius: 10,
         position: "relative",
-        // overflow: "hidden", // Hide overflow from dashed border
     },
     dashedBorder: {
         width: "100%",
         height: "100%",
-        borderColor: "darkgreen", // Dark green border color
-        borderWidth: 5, // Increase line thickness as needed
-        borderStyle: "dashed", // Dashed line style
-        borderRadius: 12, // Increase radius for larger dashes
-        marginTop: 20, // Adjust the space between dashes
-        borderSpacing: 90, // Adjust the space between dashes
+        borderColor: "darkgreen",
+        borderWidth: 5,
+        borderStyle: "dashed",
+        borderRadius: 12,
+        marginTop: 20,
+        borderSpacing: 90,
     },
     instruction: {
         fontSize: 16,
@@ -151,7 +158,15 @@ const styles = StyleSheet.create({
         marginHorizontal: 20,
         marginTop: 10,
         marginBottom: 10,
-        color: "darkgreen", // Dark green text color
+        color: "darkgreen",
+    },
+    qrCodeContainer: {
+        width: 100, // Adjust the width as needed
+        height: 200, // Adjust the height as needed
+        marginTop: 10,
+        
+        
+        
     },
 });
 
