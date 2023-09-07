7c41fc017086fa9b280b408c96b223f4d4e949ef
Amr <paadd2@hotmail.com>
2023-09-07 03:42:34 +0200

    scanning done

--- src/screens/form/QRScanner.js
+++ src/screens/form/QRScanner.js
@@ -1,15 +1,9 @@
-import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, Linking } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, Linking, Button } from "react-native";
 import { t, useLanguage } from "../../Languages/LanguageHandler";
 import { useNavigation } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/FontAwesome"; // Import the icon library you want to use
-import { RNCamera } from 'react-native-camera';
-import ItemQRCode from "../../utils/qrgen";
-import QRCode from 'react-native-qrcode-svg';
-import QRCodeScanner from 'react-native-qrcode-scanner';
-
-
-
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 
 
@@ -19,44 +13,83 @@ const QRScanner = () => {
 
     const handlePress = () => {
         navigation.goBack();
-
-
 }
-    
+
+
+const [hasPermission, setHasPermission] = useState(null);
+const [scanned, setScanned] = useState(false);
+const [text, setText] = useState('Not yet scanned');
+
+const askForCameraPermission = () => {
+    (async () => {
+        const { status } = await BarCodeScanner.requestPermissionsAsync();
+        setHasPermission(status === 'granted');
+    })();
+}
+
+// Request Camera Permission
+useEffect(() => {
+    askForCameraPermission();
+}, []);
+
+// What happens when we scan the bar code
+const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data);
+    console.log('Type: ' + type + '\nData: ' + data)
+};
+
+
+
+
   
-    return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.header}>
-                {/* Header Text */}
-                <Text style={styles.headline}>
-                    {t("QrScannerScreen.Scan", currentLanguage)}
-                </Text>
-                {/* Close Button */}
-                <TouchableOpacity style={styles.closeButton} onPress={handlePress}>
-                    <Icon size={30} name="close" style={styles.closeButtonIcon} />
-                </TouchableOpacity>
-            </View>
+return (
+    <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+            {/* Header Text */}
+            <Text style={styles.headline}>
+                {t("QrScannerScreen.Scan", currentLanguage)}
+            </Text>
+            {/* Close Button */}
+            <TouchableOpacity style={styles.closeButton} onPress={handlePress}>
+                <Icon size={30} name="close" style={styles.closeButtonIcon} />
+            </TouchableOpacity>
+        </View>
 
-            <View style={styles.content}>
-                {/* Header Instruction */}
-                <Text style={styles.instruction}>
-                    {t("QrScannerScreen.Header", currentLanguage)}
-                </Text>
+        <View style={styles.content}>
+            {/* Header Instruction */}
+            <Text style={styles.instruction}>
+                {t("QrScannerScreen.Header", currentLanguage)}
+            </Text>
 
-                {/* QR Code Scanner Frame */}
-                {/* You would integrate the QR code scanner library here */}
-                <View style={styles.qrScannerFrame}>                  
-               
-                    <View style={styles.dashedBorder}></View>
+            {/* QR Code Scanner Frame */}
+            {hasPermission ? (
+                <View style={styles.qrScannerFrame}>
+                    <BarCodeScanner
+                        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                        style={{ height: 320, width: 400 }}
+                    />
                 </View>
+            ) : (
+                <Text style={{ margin: 10 }}>No access to camera</Text>
+            )}
 
-                {/* Instruction for Non-Uptainers */}
-                <Text style={styles.instruction}>
-                    {t("QrScannerScreen.Bottom", currentLanguage)}
-                </Text>
-            </View>
-        </SafeAreaView>
-    );
+            {/* Display scanned text */}
+            <Text style={styles.maintext}>{text}</Text>
+
+            {scanned && (
+                <Button title={'Scan again?'} onPress={() => setScanned(false)} color='tomato' />
+            )}
+
+            {/* Instruction for Non-Uptainers */}
+            <Text style={styles.instruction}>
+                {t("QrScannerScreen.Bottom", currentLanguage)}
+            </Text>
+        </View>
+    </SafeAreaView>
+);
+
+
 };
 
 const styles = StyleSheet.create({
