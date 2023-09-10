139b1ec486164c037fbfd714b4715b4c8532a0df
LiYaokun97 <nukoayil@qq.com>
2023-09-10 14:58:43 +0200

    [feat] reduce padding on bodywrapper

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -4,7 +4,7 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
     BodyWrapper: {
         flex: 1,
-        paddingHorizontal: '10%',
+        paddingHorizontal: '5%',
         alignItems: "center",
         width: '100%'
     },
