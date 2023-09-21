01b070561afb4738f80f2e412ad9ca42245811cb
Attila Bako <attila.bako@outlook.com>
2023-09-21 09:41:40 +0200

    loading_reusable_component

--- App.js
+++ App.js
@@ -73,6 +73,9 @@ import ThankYouscreen from "./src/screens/Thankyouscreen";
 import {LoaderProvider}from "./src/componets/LoaderContext";
 
 
+import {LoaderProvider}from "./src/componets/LoaderContext";
+
+
 console.log("start");
 const Stack = createNativeStackNavigator();
 
