22ae8f9e71363bb45895d0f37b6bd4846b185d5f
Attila Bako <attila.bako@outlook.com>
2023-09-21 09:41:40 +0200

    loading_reusable_component

--- App.js
+++ App.js
@@ -76,6 +76,9 @@ import ChangePassword from "./src/screens/ProfileSetings/ChangePassword";
 
 import {LoaderProvider}from "./src/componets/LoaderContext";
 
+import {LoaderProvider}from "./src/componets/LoaderContext";
+
+
 console.log("start");
 const Stack = createNativeStackNavigator();
 
