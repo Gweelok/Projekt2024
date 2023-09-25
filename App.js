c2259292c2df532a1bd063f4e656af3cae549a47
Attila Bako <attila.bako@outlook.com>
2023-09-25 11:57:07 +0200

    removed_duplicate_import

--- App.js
+++ App.js
@@ -81,7 +81,7 @@ import {LoaderProvider}from "./src/componets/LoaderContext";
 import {LoaderProvider}from "./src/componets/LoaderContext";
 
 
-import {LoaderProvider}from "./src/componets/LoaderContext";
+
 
 
 console.log("start");
