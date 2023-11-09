afcdb68816d440ed9e522b3dd90c93c973a460a8
kotruta123 <315175@viauc.dk>
2023-11-09 13:09:35 +0200

    Update Stat.js

--- src/screens/Stat.js
+++ src/screens/Stat.js
@@ -104,7 +104,7 @@ const Stat = ({navigation}) => {
                             </View>
                         </View>
                     </View>
-                    <View style={[GlobalStyle, { height: 285, }]}>
+                    <View style={[ { height: 285, }]}>
                         <ChartForStats />
                     </View>
                     <View style={{ marginTop: 2, marginBottom:20}}>
@@ -141,11 +141,11 @@ const Stat = ({navigation}) => {
                         </View>
                     </View>
                     <View>
-                        <View style={[{ flexDirection: 'row', alignItems: 'center', marginTop:20, marginBottom:3,marginRight: "4%", }]}>
+                        <View style={[{ flexDirection: 'row',  marginTop:20, marginBottom:3,marginRight: "4%", }]}>
                             <LightbulbIcon />
                             <Text style={[styles.paragraph_text,{marginLeft:5}]}> {t('StatsPage.kgCO2', currentLanguage)}</Text>
                         </View>
-                        <View style={[{ flexDirection: 'row', alignItems: 'center', marginTop:3, marginBottom:3,marginRight: "4%", }]}>
+                        <View style={[{ flexDirection: 'row', marginTop:5, marginBottom:3,marginRight: "4%", }]}>
                             <LightbulbIcon />
                             <Text style={[styles.paragraph_text, {marginLeft:5}]}> {t('StatsPage.Amount', currentLanguage)} </Text>
                         </View>
