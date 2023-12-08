fd38eed6ea0efe76b79bcc9a9eadb917d2151115
nadia <nadia.gincoff@gmail.com>
2023-12-08 13:10:44 +0100

    replace reusable searchbox and delete unusable code

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,15 @@
-import { View, TextInput, StyleSheet, Dimensions } from "react-native";
-import { Backgroundstyle, Primarycolor1 } from "../styles/Stylesheet";
+import { View } from "react-native";
+import { Backgroundstyle } from "../styles/Stylesheet";
 import Navigationbar from "../componets/Navigationbar";
 import React, { useState } from "react";
 import * as Location from "expo-location";
 import SortUptainers from "../componets/sortUptainers";
-import { Feather } from "@expo/vector-icons";
 import GlobalStyle from "../styles/GlobalStyle";
-import ScrollViewComponent from "../componets/atoms/ScrollViewComponent";
+import SearchBox from '../componets/SearchBox';
 import { firebaseAurth } from "../utils/Firebase";
 
 const Home = ({ navigation }) => {
+  const [search, onChangeSearch] = useState("");
   //Asks for premission to use location at home screen only, must be sent here for new users or copy paste to other screens
   console.log("start current useeffect " + firebaseAurth.currentUser);
   (async () => {
@@ -23,55 +23,19 @@ const Home = ({ navigation }) => {
     }
   })();
 
-  const [search, onChangeSearch] = useState("");
-
   return (
     <View style={[Backgroundstyle.interactive_screens]}>
       <View style={GlobalStyle.BodyWrapper}>
-        <View style={styles.container}>
-          <TextInput
-            style={styles.input}
-            placeholder="Search"
-            onChangeText={onChangeSearch}
-            value={search}
-          />
-          <Feather
-            style={styles.searchIcon}
-            name="search"
-            size={24}
-            color={Primarycolor1}
-          />
-        </View>
-        {/*ScrollViewComponent is deleted here and moved to sortUptainer*/}
+        <SearchBox
+          onChangeText={onChangeSearch}
+          value={search}
+          placeholderText={"SearchField.productPlaceholder"}
+        /> 
         <SortUptainers navigation={navigation} />
-
         <Navigationbar navigation={navigation} />
       </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  searchIcon: {
-    position: "absolute",
-    right: 10,
-    top: "50%",
-    transform: [{ translateY: -12 }],
-  },
-  container: {
-    marginTop: 15,
-    width: "100%",
-    // width:  Dimensions.get('window').width * 0.9,
-    // marginLeft : "auto",
-    // marginRight:"auto",
-    backgroundColor: "#fff",
-    marginBottom: 15,
-  },
-  input: {
-    height: 40,
-    borderColor: Primarycolor1,
-    borderWidth: 3,
-    paddingHorizontal: 10,
-  },
-});
 export default Home;
