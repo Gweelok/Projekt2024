54c92d33bce481a7aeb41a3ad39a986fc79aa5e8
amineaouini-notbot <aminaouini565@gmail.com>
2023-12-18 20:16:23 +0100

    use already setup algorithm

--- src/utils/uptainersUtils.js
+++ src/utils/uptainersUtils.js
@@ -1,13 +1,14 @@
-export const calculateDistance = async (userLat, userLon, uptainerLat, uptainerLon) =>{
-    let R = 6371.0710; // Radius of the Earth in KM
-    let rlat1 = userLat * (Math.PI/180); // Convert degrees to radians
-    let rlat2 = uptainerLat * (Math.PI/180); // Convert degrees to radians
-    let difflat = rlat2-rlat1; // Radian difference (latitudes)
-    let difflon = (uptainerLon-userLon) * (Math.PI/180); // Radian difference (longitudes)
-
-    let distance = 2 * R * 
-    Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)
-        +Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
-    return distance;
-    
-}
+export const calculateDistance = (lat1, lon1, lat2, lon2) => {
+    const R = 6371;
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+    const dLon = (lon2 - lon1) * (Math.PI / 180);
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos((lat1 * Math.PI) / 180) *
+        Math.cos((lat2 * Math.PI) / 180) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c;
+    return distance.toFixed(2);
+};
