94016a0276bafdab4a501710ee5e2a7cbe0cbaa8
amineaouini-notbot <aminaouini565@gmail.com>
2023-12-18 23:25:13 +0100

    isolate searched locations component

---
+++ src/screens/map/mapDisplay/SearchedLocation.js
@@ -0,0 +1,71 @@
+import React from "react";
+import {
+    dropdownStyles,
+    Primarycolor1,
+    Primarycolor2,
+    Primarycolor3,
+    Primarycolor4,
+    styles
+} from "../../../styles/Stylesheet";
+import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
+import { calculateDistance } from "../../../utils/uptainersUtils";
+
+const SearchedLocation = ({location, styling, onPress, userLatitude, userLongitude,}) =>{
+    return (
+    <TouchableOpacity
+        key={location.uptainerName}
+        onPress={onPress}
+        style={styling}
+    >
+        <View style={styles1.stationInfo}>
+            <View>
+                <Text style={styles1.stationName}>{location.uptainerName}</Text>
+                <View style={styles1.addressInfo}>
+                    <Text style={[styles.article_text, styles1.stationAddress]}>{`${location.uptainerStreet}, ${location.uptainerCity}`}</Text>
+                    <View style={styles1.spacer} />
+                    <Text style={styles1.distance}>{` ${calculateDistance(
+                        userLatitude,
+                        userLongitude,
+                        parseFloat(location.uptainerLat),
+                        parseFloat(location.uptainerLong)
+                    )} km`}</Text>
+                </View>
+            </View>
+        </View>
+    </TouchableOpacity>)
+}
+
+const styles1 = StyleSheet.create({
+    stationInfo: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+
+    },
+    stationName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        marginBottom: 10,
+        color: Primarycolor1,
+    },
+    addressInfo: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10,
+    },
+    stationAddress: {
+        fontSize: 12,
+        color:Primarycolor1,
+        width:"75%"
+    },
+
+    distance: {
+        width:"25%",
+        fontSize: 12,
+        color: Primarycolor1,
+        alignItems:"center"
+    },
+
+});
+
+export default SearchedLocation;
