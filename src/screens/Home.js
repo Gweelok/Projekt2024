a4f119cfc5b40ec092854f5aae65b57e62d77a63
amineaouini-notbot <aminaouini565@gmail.com>
2023-12-22 16:09:33 +0100

    setup home route

---
+++ src/screens/Home.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native"
+
+const Home = () => {
+    return (
+        <View>
+            <Text>
+                Homedd  
+            </Text>
+        </View>
+    )
+}
+
+export default Home
