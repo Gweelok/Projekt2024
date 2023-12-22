7a0f9a3ca951e8bb7ed0a296740c1f0f0ad22c26
amineaouini-notbot <aminaouini565@gmail.com>
2023-12-22 21:04:25 +0100

    finalizing task

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screens/Home';
 
 export default function App() {
+  const Stack = createNativeStackNavigator()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen options={{headerShown: false}} name="Home" component={Home}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
