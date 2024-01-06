86828bb32e6112ffd7d8093dc38934d1e6bbc4d1
amineaouini-notbot <aminaouini565@gmail.com>
2024-01-06 23:32:25 +0100

    stylish searched products

--- src/componets/ItemsSearched.js
+++ src/componets/ItemsSearched.js
@@ -3,6 +3,8 @@ import { Pressable, StyleSheet, View, Text, Image } from "react-native"
 import { getItemById, getUptainerById } from "../utils/Repo"
 import { getDownloadURL, getStorage, ref } from "firebase/storage"
 import { calculateDistance } from "../utils/uptainersUtils"
+import { windowWidth } from "../utils/Dimensions"
+import { Primarycolor1, Primarycolor2, Primarycolor3 } from "../styles/Stylesheet"
 
 const ItemsSearched = ({navigation, product, item, index, userLocation}) =>{
     const [imageUrl, setImageUrl] = useState(null)
@@ -12,7 +14,6 @@ const ItemsSearched = ({navigation, product, item, index, userLocation}) =>{
         (async ()=>{
             const getUptainer = await getUptainerById(item.itemUptainer)
             const imageRef = ref(storage, item.itemImage)
-            console.log(getUptainer)
             const image = await getDownloadURL(imageRef)
             setImageUrl(image)
             setUptainer(getUptainer)
@@ -21,28 +22,51 @@ const ItemsSearched = ({navigation, product, item, index, userLocation}) =>{
     }, [])
 
     return (
-        <Pressable>
+        <Pressable style={style.mainContainer} key={index}>
             <View>
-                <View>
-                    <Text>{product.productName}</Text>
-                    {(!!userLocation && !!uptainer) && <Text>{
+                <View style={style.container1}>
+                    <Text style={style.productName}>{product.productName}</Text>
+                    {(!!userLocation && !!uptainer) && <Text style={style.distance}>{
                         calculateDistance(
                         {latitude: userLocation.latitude, longitude: userLocation.longitude},
                         {latitude: uptainer.uptainerLat, longitude: uptainer.uptainerLong}
                         )}</Text>}
                 </View>
-                {!!uptainer &&<Text>{uptainer.uptainerName} / {uptainer.uptainerStreet}</Text>}
+                {!!uptainer &&<Text style={style.uptainer}>{uptainer.uptainerName || uptainer} / {uptainer.uptainerStreet}</Text>}
                 {!!imageUrl && <Image source={{uri: imageUrl}} style={style.image}></Image>}
-                <Text>--</Text>
             </View>
         </Pressable>
     )
 }
 
 const style = StyleSheet.create({
+    mainContainer: {
+        width: windowWidth * 0.9,
+        marginTop: 8
+    },
+    container1: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginRight: 5
+        
+    },
+    productName: {
+        color: Primarycolor1,
+        fontSize: 16
+    },
+    uptainer: {
+        color: Primarycolor1,
+        fontSize: 13
+    },
+    distance: {
+        color: Primarycolor1,
+        fontSize: 11
+    },
     image: {
-        width: 120,
-        height: 120,
+        marginTop: 5,
+        width: 100,
+        height: 100,
     }
 })
 
