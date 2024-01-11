9a02efe8684425d469d82057d8e4fa4a9369009a
Paul Lindén <linden.91.pl@gmail.com>
2024-01-11 11:37:09 +0100

    changed back inital route

--- App.js
+++ App.js
@@ -2,17 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './src/screens/Home';
-
-//Change back to home
-import ServiceAdmin from './src/screens/ServiceAdmin'
+import Home from './src/screens/Home'
 
 export default function App() {
   const Stack = createNativeStackNavigator()
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='ServiceAdmin'>
-        <Stack.Screen options={{headerShown: false}} name="ServiceAdmin" component={ServiceAdmin}/>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen options={{headerShown: false}} name="Home" component={Home}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
