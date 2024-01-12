e6afb23a7793494ec9b806de48efcd3a4c11d958
Paul Lindén <linden.91.pl@gmail.com>
2024-01-12 16:22:54 +0100

    Moved files to home folder

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,6 @@
 import { View, StyleSheet } from "react-native"
 import HomeContent from "../components/Home/HomeContent"
 import { windowHeight, windowWidth } from "../utils/Dimensions"
-import UptainerButtonList from "../components/Home/UptainerButtonList"
 
 const Home = () => {
     return (
