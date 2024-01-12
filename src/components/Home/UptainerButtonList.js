e0b97095b2a1dca5c0224cd1d4620ffe75da066f
Paul Lindén <linden.91.pl@gmail.com>
2024-01-12 14:41:13 +0100

    Updated Async for list

--- src/components/Home/UptainerButtonList.js
+++ src/components/Home/UptainerButtonList.js
@@ -1,28 +1,18 @@
 import * as Location from 'expo-location';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, FlatList, ActivityIndicator, } from 'react-native';
-import { getAllUptainers, signInUser } from "../../utils/Repo";
+import { getAllUptainers} from "../../utils/Repo";
 import { dropdownStyles } from "../../styles/styleSheet";
 import GlobalStyle from "../../styles/GlobalStyle"
 import { calculateDistance } from '../../utils/uptainersUtils';
 import UptainerLocation from './UptainerLocationButton';
 
-/****************************************
-Only for purpose of reaching db in test,
-add user......
-Needs to be removed later on.   
-****************************************/
-signInUser('', '');
-/***************************************
-***************************************/
-
-
 const UptainerButtonList = () => {
-    const [filteredLocations, setFilteredLocations] = useState(filteredLocations);
+    const [filteredLocations, setFilteredLocations] = useState([]);
     const [userLocation, setUserLocation] = useState(null);
     const [loading, setLoading] = useState(true);
 
-    const fetchData = async () => {
+    const getUptainers = async () => {
         try {
             const uptainerList = await getAllUptainers();
             setFilteredLocations(uptainerList);
@@ -31,61 +21,20 @@ const UptainerButtonList = () => {
         }
     };
 
-    useEffect(() => {
-        fetchData();
-
-        const getUserLocation = async () => {
-            try {
-                const { status } = await Location.requestForegroundPermissionsAsync();
-                if (status !== 'granted') {
-                    // Handle denied permission
-                    setLoading(false);
-                    return;
-                }
-
-                const location = await Location.getCurrentPositionAsync({});
-                setUserLocation(location.coords);
+    const getUserLocation = async () => {
+        try {
+            const { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
                 setLoading(false);
-            } catch (error) {
-                console.error('Error fetching location:', error);
-                setLoading(false);
+                return;
             }
-        };
-
-        getUserLocation();
-    }, []);
-
-    if (loading) {
-        return (
-            <View>
-                <ActivityIndicator size='large' color='black' />
-            </View>
-        );
-    }
-
-    const userLatitude = userLocation?.latitude || 0;
-    const userLongitude = userLocation?.longitude || 0;
-
-    const renderItem = ({ item, index }) => {
-        const distance = calculateDistance(
-            { latitude: userLatitude, longitude: userLongitude },
-            { latitude: parseFloat(item.uptainerLat), longitude: parseFloat(item.uptainerLong) }
-        );
-
-        return (
-            <UptainerLocation
-                location={item}
-                onPress={() => handleUptainerPress(item)}
-                index={index}
-                styling={[
-                    dropdownStyles.dropdownListItem2,
-                    index === filteredLocations.length - 1 ? styles1.lastItem : null,
-                ]}
-                userLatitude={userLatitude}
-                userLongitude={userLongitude}
-                distance={distance}
-            />
-        );
+            const location = await Location.getCurrentPositionAsync({});
+            setUserLocation(location.coords);
+            setLoading(false);
+        } catch (error) {
+            console.error('Error fetching location:', error);
+            setLoading(false);
+        }
     };
 
     const handleUptainerPress = (location) => {
@@ -93,18 +42,66 @@ const UptainerButtonList = () => {
         console.log(`Uptainer ${location.uptainerName} pressed`);
     };
 
+    useEffect(() => {
+        async function getLocationAndData() {
+            await getUserLocation();
+            await getUptainers();
+        }
+        getLocationAndData();
+    }, []);
+
+    const renderUptainers = ({ item, index }) => {
+        const userLatitude = userLocation?.latitude || 0;
+        const userLongitude = userLocation?.longitude || 0;
+
+        const uptainerLatitude = item.uptainerLatitude;
+        const uptainerLongitude = item.uptainerLongitude;
+
+        const distance = calculateDistance(
+            { latitude: userLatitude, longitude: userLongitude },
+            { latitude: parseFloat(uptainerLatitude), longitude: parseFloat(uptainerLongitude) }
+        );
+
+        return (
+            <UptainerLocation
+                key={index}
+                location={item}
+                onPress={() => handleUptainerPress(item)}
+                index={index}
+                styling={[
+                    dropdownStyles.dropdownListItem2,
+                    index === filteredLocations.length - 1 ? styles1.lastItem : null,
+                ]}
+                distance={distance}
+            />
+        );
+    };
+
     return (
         <View style={styles1.container}>
-            <FlatList
-                data={filteredLocations}
-                keyExtractor={(item) => item.uptainerName}
-                style={[GlobalStyle.BodyWrapper, dropdownStyles.dropdownContainer2]}
-                renderItem={renderItem}
-            />
+            {loading ? (
+                <ActivityIndicator size='large' color='black' />
+            ) : (
+                <FlatList
+                    data={filteredLocations}
+                    keyExtractor={(item) => item.uptainerName}
+                    style={[GlobalStyle.BodyWrapper, styles1.uptainerList]}
+                    renderItem={renderUptainers} 
+                />
+            )}
         </View>
     );
 };
 
-const styles1 = StyleSheet.create({ lastItem: { borderBottomWidth: 3 } });
+const styles1 = StyleSheet.create(
+    {
+        lastItem: { borderBottomWidth: 3 },
+        uptainerList: {
+            position: 'absolute',
+            zIndex: 1,
+            marginTop: 87,
+            width: '100%',
+        }
+    });
 
 export default UptainerButtonList;
