117e47ff4c4ee1511eb615871f0242aeb2b4bdc4
Paul Lindén <linden.91.pl@gmail.com>
2024-01-16 09:24:57 +0100

    Added functions for handling search

--- src/components/Home/ServiceAdminContent.js
+++ src/components/Home/ServiceAdminContent.js
@@ -1,27 +1,40 @@
 import { View, StyleSheet } from "react-native"
+import { useState } from 'react';
 import UptainerList from "./UptainerList"
 import SearchBox from "./SearchBox"
 import { windowHeight, windowWidth } from "../../utils/Dimensions"
 import GlobalStyle from "../../styles/GlobalStyle"
-import { windowHeight, windowWidth } from "../../utils/Dimensions"
 
 const ServiceAdmin = () => {
+    const [searchText, setSearchText] = useState('');
+    const [isSearching, setIsSearching] = useState(false);
+
+    //Function to open the search when the search box is touched.
+    const openSearch = () => {
+        setIsSearching(true)
+    };
+    //Function to close the search when the user touches outside the search box.
+    const closeSearch = () => {
+        setIsSearching(false)
+    };
+
     return (
         <View style={style.container}>
 
             <View style={style.container2} >
 
-                <View style={[GlobalStyle.BodyWrapper, style.searchBox]} >
-                    <SearchBox value={searchText} placeholderText={"Search"}></SearchBox>
+                <View style={[GlobalStyle.BodyWrapper, style.searchBox]} onTouchStart={openSearch}>
+                    <SearchBox onChangeText={(text) => setSearchText(text)} value={searchText} placeholderText={"Search"}></SearchBox>
                 </View>
 
-
-                <View>
-                    <UptainerList searchValue={searchText}></UptainerList>
-                </View>
+                {isSearching && (
+                    <View>
+                        <UptainerList searchValue={searchText}></UptainerList>
+                    </View>
+                )}
 
             </View>
-            <View>
+            <View onTouchStart={closeSearch} style={style.container3}>
 
             </View>
         </View>
