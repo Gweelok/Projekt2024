83ab126d578650d4a8d64f98b590ace8691bedae
Paul Lindén <linden.91.pl@gmail.com>
2024-01-16 09:12:38 +0100

    Added components and styles for search field

--- src/components/Home/ServiceAdminContent.js
+++ src/components/Home/ServiceAdminContent.js
@@ -1,11 +1,29 @@
 import { View, StyleSheet } from "react-native"
 import UptainerList from "./UptainerList"
+import SearchBox from "./SearchBox"
+import { windowHeight, windowWidth } from "../../utils/Dimensions"
+import GlobalStyle from "../../styles/GlobalStyle"
 import { windowHeight, windowWidth } from "../../utils/Dimensions"
 
 const ServiceAdmin = () => {
     return (
         <View style={style.container}>
-            <UptainerList></UptainerList>
+
+            <View style={style.container2} >
+
+                <View style={[GlobalStyle.BodyWrapper, style.searchBox]} >
+                    <SearchBox value={searchText} placeholderText={"Search"}></SearchBox>
+                </View>
+
+
+                <View>
+                    <UptainerList searchValue={searchText}></UptainerList>
+                </View>
+
+            </View>
+            <View>
+
+            </View>
         </View>
     )
 }
@@ -13,7 +31,23 @@ const ServiceAdmin = () => {
 const style = StyleSheet.create({
     container: {
         height: windowHeight,
-        width: windowWidth
+        width: windowWidth,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    container2: {
+        width: '80%',
+    },
+    container3: {
+        width: '100%',
+        height: '100%'
+    },
+    searchBox: {
+        position: 'absolute',
+        zIndex: 1,
+        marginTop: 50,
+        width: '100%',
+        flexDirection: 'row',
     }
 })
 
