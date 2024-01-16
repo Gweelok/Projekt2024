7cb385eaf868cbc00e5b1e999d1dca6a54646f37
Paul Lindén <linden.91.pl@gmail.com>
2024-01-16 09:00:21 +0100

    Added SearchBox from user app

---
+++ src/components/Home/SearchBox.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {View, TextInput, StyleSheet} from 'react-native';
+
+import {Feather} from "@expo/vector-icons";
+import {Primarycolor1, Primarycolor4} from "../../styles/styleSheet";
+
+const SearchBox = ({ onChangeText, value, placeholderText }) => {
+   
+    return (
+        <View style={styles.container}>
+            <TextInput
+                style={styles.input}
+                onChangeText={onChangeText}
+                value={value}
+                placeholder={placeholderText}
+                placeholderTextColor={Primarycolor4}
+            />
+            <Feather 
+                style={styles.searchIcon} 
+                name="search" 
+                size={24} 
+                color={Primarycolor4} 
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    searchIcon: {
+        position: 'absolute',
+        right: 10,
+        top: '50%',
+        transform: [{ translateY: -12 }],
+    },
+    container: {
+        width: '100%',
+        alignSelf: 'center',
+        backgroundColor: '#fff',
+        marginBottom: 15,
+    },
+    input: {
+        height: 40,
+        borderColor: Primarycolor1,
+        borderWidth: 3,
+        paddingHorizontal: 10,
+    },
+});
+
+export default SearchBox;
