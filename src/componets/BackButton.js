869bc3c8a56d8fb72c82b71d412cb9809a2a0c40
IbrahimIsaDemir <ibdem20@student.sdu.dk>
2024-01-22 17:13:50 +0100

    removed unused comma

--- src/componets/BackButton.js
+++ src/componets/BackButton.js
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
         height: 40,
         justifyContent: "center",
         alignItems: "center",
-        marginRight: 10,
+        marginRight: 10
       }
 });
 export default BackButton;
