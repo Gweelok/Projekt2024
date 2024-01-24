fe16dd56f98dbacb69109175e005fb732749117c
Paul Lindén <linden.91.pl@gmail.com>
2024-01-24 15:45:46 +0100

    Added font load from main app.

--- App.js
+++ App.js
@@ -2,20 +2,38 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from "expo-font";
+
+import { LanguageProvider } from './src/Languages/LanguageHandler';
+
+//importing pages for navigation
 import Home from './src/screens/Home';
 import Login from './src/screens/Login';
-import { LanguageProvider } from './src/Languages/LanguageHandler';
 import Uptainer from './src/screens/Uptainer';
 
+
 export default function App() {
+
+  //Loading the font
+  const [fontsLoaded] = useFonts({
+    "space-grotesk": require("./assets/fonts/SpaceGrotesk-Regular.ttf"),
+    "space-grotesk-bold": require("./assets/fonts/SpaceGrotesk-Bold.ttf"),
+    "space-grotesk-Medium": require("./assets/fonts/SpaceGrotesk-Medium.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    // Font is not yet loaded, return null or a loading screen
+    return null;
+  }
+
   const Stack = createNativeStackNavigator()
   return (
     <LanguageProvider>
       <NavigationContainer>
         <Stack.Navigator initialRouteName='Login'>
-          <Stack.Screen options={{headerShown: false}} name="Home" component={Home}/>
-          <Stack.Screen options={{headerShown: false}} name="Login" component={Login}/>
-          <Stack.Screen options={{headerShown: false}} name="Uptainer" component={Uptainer}/>
+          <Stack.Screen options={{ headerShown: false }} name="Home" component={Home} />
+          <Stack.Screen options={{ headerShown: false }} name="Login" component={Login} />
+          <Stack.Screen options={{ headerShown: false }} name="Uptainer" component={Uptainer} />
         </Stack.Navigator>
       </NavigationContainer>
     </LanguageProvider>
