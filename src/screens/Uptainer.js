7a4add289ab12598f57ba50b18d90b5d0095bf76
Paul Lindén <linden.91.pl@gmail.com>
2024-01-24 10:24:46 +0100

    fixed file path

--- src/screens/Uptainer.js
+++ src/screens/Uptainer.js
@@ -1,6 +1,6 @@
 import { View, StyleSheet } from "react-native"
 import { windowHeight, windowWidth } from "../utils/Dimensions"
-import UptainerContent from "../components/Uptainer/UptainerContent"
+import UptainerContent from "../components/uptainer/UptainerContent"
 
 const Uptainer = ({route}) => {
     const { location } = route.params;
