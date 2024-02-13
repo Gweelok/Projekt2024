f836e94f0f8e2257fb11f48459c2ec4aea82f402
amineaouini-notbot <aminaouini565@gmail.com>
2024-02-13 13:50:42 +0100

    setup add uptainer form

--- src/screens/AddUptainer.js
+++ src/screens/AddUptainer.js
@@ -2,7 +2,7 @@ import { StyleSheet, View } from "react-native";
 import UptainerInfo from "../components/atoms/UptainerInfo";
 import GlobalStyle from "../styles/GlobalStyle";
 import { windowHeight, windowWidth } from "../utils/Dimensions";
-
+import AddUptainerForm from "../components/AddUptainer/AddUptainerForm";
 
 export default function AddUptainer ({ route }) {
     const {location} = route.params;
@@ -10,6 +10,7 @@ export default function AddUptainer ({ route }) {
     return (
         <View style={[style.container, GlobalStyle.BodyWrapper]}>
             <UptainerInfo location={location}/>
+            <AddUptainerForm></AddUptainerForm>
         </View>
     )
 } 
