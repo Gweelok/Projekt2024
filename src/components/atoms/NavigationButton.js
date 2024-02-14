6f53bfef260848b6bd0a52820d2ed56f4b141574
Paul Lindén <linden.91.pl@gmail.com>
2024-02-14 14:44:24 +0100

    Updated navbutton to also take callback if needed

--- src/components/atoms/NavigationButton.js
+++ src/components/atoms/NavigationButton.js
@@ -9,20 +9,24 @@ import { useNavigation } from "@react-navigation/native";
     @path = the navigation path 
     @text = the text that will be displayed in button
     @location = param for passing location props if needed
+
+    (Optional)
+    @callback = pass function to button that will be executed before navigating
 */
 
-const NavgationButton = (({ path, text, location, buttonStyle, textStyle }) => {
+const NavgationButton = (({ path, text, location, buttonStyle, textStyle, callback }) => {
     //access the navigation prop of the parent screen
     const navigation = useNavigation();
     
-    function handelPress() {
-        console.log(path)
-        console.log(location)
+    function handlePress() {
+        if (callback) {
+            callback();
+        }
         navigation.navigate(path, { location })
     }
 
     return (
-        <TouchableOpacity onPress={() => handelPress()} style={buttonStyle}>
+        <TouchableOpacity onPress={() => handlePress()} style={buttonStyle}>
             <Text style={textStyle}>{text}</Text>
         </TouchableOpacity>
     )
