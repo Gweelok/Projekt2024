1b0848e0ddbb72c0222f33ec661ec0a554443356
mura0717 <mura0717@stud.kea.dk>
2024-02-20 14:49:31 +0100

    info C02: icon added, text added, X button added and implemented. But styling is not right yet

--- src/screens/InfographicCO2Screen.js
+++ src/screens/InfographicCO2Screen.js
@@ -1,36 +1,94 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { styles, Backgroundstyle } from "../styles/Stylesheet";
-import { useLanguage, t } from "../Languages/LanguageHandler"; 
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Pressable,
+} from "react-native";
+import BackButton from "../componets/BackButton";
+import {
+  Buttons,
+  styles,
+  Backgroundstyle,
+  Primarycolor1,
+  Primarycolor2,
+  Primarycolor3,
+} from "../styles/Stylesheet";
+import GlobalStyle from "../styles/GlobalStyle";
+import { useLanguage, t } from "../Languages/LanguageHandler";
+import CloudSvg from "../componets/svg-components/CloudCO2";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 const InfographicCO2Screen = ({ navigation }) => {
-    return (
-      <View style={[Backgroundstyle.message_Screens]}>
-        <View style={Backgroundstyle.interactive_screens}>
-          <BackButton onPress={navigation.goBack} style={Buttons.backButton} />
-          <Text style={styles.Header_Primarycolor1}> Info page </Text>
+  const { currentLanguage } = useLanguage();
+  return (
+    <View style={[Backgroundstyle.informationScreens, GlobalStyle.BodyWrapper]}>
+      <View>
+        <View style={infoGraphicStyle.cancelIconContainer}>
+          <TouchableOpacity>
+            <Ionicons
+              onPress={() => navigation.navigate("Homepage")}
+              name="close-outline"
+              size={30}
+              color="white"
+              style={infoGraphicStyle.cancelIcon}
+            />
+          </TouchableOpacity>
         </View>
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            marginTop: 250,
-            marginBottom: 250,
-          }}
-        >
-          <Text
+        <View style={infoGraphicStyle.svgContainer}>
+          <View>
+            <CloudSvg iconText="C02" textStyle={infoGraphicStyle.iconText} />
+          </View>
+          <View
             style={{
-              color: Primarycolor3,
-              fontSize: 20,
-              margin: 20,
-              textAlign: "center",
+              justifyContent: "center",
+              marginTop: 50,
+              marginBottom: 50,
             }}
           >
-            {t("thankYouScreen.header", currentLanguage)}
-          </Text>
+            <Text
+              style={infoGraphicStyle.messageText}
+            >
+              Product successfully taken.
+              {/* {t("thankYouScreen.header", currentLanguage)} */}
+            </Text>
+          </View>
         </View>
       </View>
-    );
+    </View>
+  );
 };
 
 export default InfographicCO2Screen;
+
+const infoGraphicStyle = StyleSheet.create({
+  svgContainer: {
+    width: "100%",
+    alignItems: "center",
+    marginTop: 50,
+  },
+  cancelIcon: {
+    // top: 20,
+  },
+  cancelIconContainer: {
+    position: "absolute",
+    zIndex: 999,
+    right: 0,
+    backgroundColor: Primarycolor1,
+  },
+  iconText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+    zIndex: 100,
+    position: "absolute",
+  },
+  messageText: {
+    color: Primarycolor1,
+    fontSize: 20,
+    margin: 20,
+    textAlign: "center",
+  },
+});
