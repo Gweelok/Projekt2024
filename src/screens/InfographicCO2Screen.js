5dd88e782c537700b43808d30761f25f2ee8aa2f
mura0717 <mura0717@stud.kea.dk>
2024-02-23 15:56:17 +0100

    updated fontFamily & close button

--- src/screens/InfographicCO2Screen.js
+++ src/screens/InfographicCO2Screen.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, useContext } from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView } from "react-native";
 import { styles, Backgroundstyle, Primarycolor1 } from "../styles/Stylesheet";
 import GlobalStyle from "../styles/GlobalStyle";
 import { useLanguage, t } from "../Languages/LanguageHandler";
 import CloudSvg from "../componets/svg-components/Cloud";
-import { Ionicons } from "@expo/vector-icons";
+import Icon from "react-native-vector-icons/AntDesign";
 import { LoaderContext } from "../componets/LoaderContext";
 import LoadingScreen from "../componets/LoadingScreen";
 
@@ -27,16 +27,14 @@ const InfographicCO2Screen = ({ navigation }) => {
 
   return (
     <View style={[GlobalStyle.BodyWrapper, Backgroundstyle.informationScreens]}>
-      <View >
+      <View>
         {isLoading && <LoadingScreen isLoaderShow={isLoading} />}
         <View style={infoGraphicC02Style.cancelIconContainer}>
-          <TouchableOpacity>
-            <Ionicons
-              onPress={() => navigation.navigate("Homepage")}
-              name="close-outline"
-              size={30}
-              color="white"
-            />
+          <TouchableOpacity
+            style={styles.closeButton}
+            onPress={() => navigation.navigate("Homepage")}
+          >
+            <Icon size={30} name="close" style={styles.closeButtonIcon} />
           </TouchableOpacity>
         </View>
         <View style={infoGraphicC02Style.svgContainer}>
@@ -64,7 +62,7 @@ const infoGraphicC02Style = StyleSheet.create({
   svgContainer: {
     width: "100%",
     alignItems: "center",
-    marginTop: "35%",
+    marginTop: "40%",
   },
   cancelIconContainer: {
     position: "absolute",
@@ -81,5 +79,6 @@ const infoGraphicC02Style = StyleSheet.create({
     color: Primarycolor1,
     fontSize: 22,
     textAlign: "center",
+    fontFamily: "space-grotesk",
   },
 });
